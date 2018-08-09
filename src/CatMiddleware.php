ea895a2076b835946e7e0850f15ea93e6605498e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-08-09 23:18:21 +0200

    Corrected namespace :X

--- src/CatMiddleware.php
+++ src/CatMiddleware.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace WyriHaximus\Psr15\EtcPasswd;
+namespace WyriHaximus\Psr15\Cat;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
